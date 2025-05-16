cab7bdf07aa2867fa27b59daee2875bd4dd4d831
parker0509 <167055018+parker0509@users.noreply.github.com>
2025-05-16 23:15:59 +0900

    feat: funding-service-api

---
+++ funding-service/funding-service/src/main/java/com/example/funding_service/FundingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.funding_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundingServiceApplication.class, args);
+	}
+
+}
---
+++ funding-service/funding-service/src/test/java/com/example/funding_service/FundingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.funding_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
